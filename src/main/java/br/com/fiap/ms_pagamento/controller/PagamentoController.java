01131a85d6a1f23de0bcdc36b80af3b47eda9fe9
Laboratório FIAP <labsfiap@fiap.com.br>
2024-08-14 09:04:07 -0300

    start docs sketch

--- src/main/java/br/com/fiap/ms_pagamento/controller/PagamentoController.java
+++ src/main/java/br/com/fiap/ms_pagamento/controller/PagamentoController.java
@@ -2,11 +2,15 @@ package br.com.fiap.ms_pagamento.controller;
 
 import br.com.fiap.ms_pagamento.dto.PagamentoDTO;
 import br.com.fiap.ms_pagamento.service.PagamentoService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.ServletRequestBindingException;
 import org.springframework.web.bind.annotation.*;
@@ -17,18 +21,26 @@ import java.util.List;
 
 @RestController
 @RequestMapping(value = "/pagamentos")
+@Tag(name = "Pagamentos", description = "Controller/Publisher dos Pagamentos")
 public class PagamentoController {
 
     @Autowired
     private PagamentoService pagamentoService;
 
-    @GetMapping
+    @Operation(description = "Listar todos os pagamentos",
+            summary = "Retorna uma lista de pagamentos",
+            responses = {@ApiResponse(description = "Ok", responseCode = "200")})
+    @GetMapping(produces = "application/json")
     public ResponseEntity<Page<PagamentoDTO>> findAll(@PageableDefault(size = 10) Pageable pageable) {
         Page<PagamentoDTO> dto = pagamentoService.findAll(pageable);
         return ResponseEntity.ok(dto);
     }
 
-    @GetMapping("/{id}")
+    @Operation(description = "Retorna um pagamento a partir do identificador {id}",
+            summary = "Retorna um pagamento por id",
+            responses = {@ApiResponse(description = "Ok", responseCode = "200"),
+                         @ApiResponse(description = "Not found", responseCode = "404")})
+    @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<PagamentoDTO> findById(@PathVariable Long id) {
         return ResponseEntity.ok(pagamentoService.findById(id));
     }
